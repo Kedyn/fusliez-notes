46fa96b2b4d0f4ea8eb3106586c89dff2d49abaa
Kedyn Macedonio <mkedyn@gmail.com>
2020-09-22 13:40:30 -0700

    Changes Maps with no text and adds svg components

---
+++ src/components/MapsContent/MiraHq.tsx
@@ -0,0 +1,236 @@
+import React from "react";
+
+export default function MiraHq(
+  props: React.SVGProps<SVGSVGElement>
+): JSX.Element {
+  return (
+    <svg viewBox="0 0 1366 768" {...props}>
+      <title>{"MiraHq"}</title>
+      <image
+        fill="#b3cde0"
+        href="assets/Mirahq.png"
+        preserveAspectRatio="xMidYMid meet"
+      />
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={106}
+        x={842.5}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Greenhouse"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={154}
+        x={936.5}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Admin"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={232}
+        x={794.5}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Office"}
+      </text>
+      <text
+        transform="rotate(45.439 727.06 812.293) scale(.78224 1)"
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={551.511}
+        x={705.185}
+        strokeOpacity="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Communications"}
+      </text>
+      <text
+        transform="rotate(67.008 640.961 672.111) scale(.83183 1)"
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={583.612}
+        x={630.578}
+        fillOpacity="null"
+        strokeOpacity="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Locker Room"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={651}
+        x={798.5}
+        strokeOpacity="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"MedBay"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={544}
+        x={1054.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Cafeteria"}
+      </text>
+      <text
+        transform="rotate(45 938.725 548.208)"
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={555}
+        x={901.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Storage"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={699}
+        x={1047.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Balcony"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={629}
+        x={310.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Launchpad"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={328}
+        x={492.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Reactor"}
+      </text>
+      <text
+        transform="rotate(45 705.725 339.208)"
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={346}
+        x={653.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Laboratory"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={280}
+        x={264.5}
+        strokeOpacity="null"
+        strokeWidth={0}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Vents"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={315}
+        x={265}
+        strokeOpacity="null"
+        strokeWidth={0}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Vent Connections"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={345}
+        x={265}
+        strokeOpacity="null"
+        strokeWidth={0}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Wiring Tasks"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={380}
+        x={264.5}
+        strokeOpacity="null"
+        strokeWidth={0}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Common Tasks"}
+      </text>
+    </svg>
+  );
+}
---
+++ src/components/MapsContent/Polus.tsx
@@ -0,0 +1,365 @@
+import React from "react";
+
+export default function Polus(
+  props: React.SVGProps<SVGSVGElement>
+): JSX.Element {
+  return (
+    <svg viewBox="0 0 1366 768" {...props}>
+      <title>{"Polus"}</title>
+      <image
+        fill="#b3cde0"
+        href="assets/Polus.png"
+        preserveAspectRatio="xMidYMid meet"
+      />
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={368}
+        x={334.5}
+        strokeOpacity="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Electrical"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={355}
+        x={189.5}
+        strokeOpacity="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Security"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={435}
+        x={813.5}
+        strokeOpacity="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"1. EAST"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={361}
+        x={631.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Storage"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={266}
+        x={929.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Laboratory"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={597}
+        x={1013.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Specimen Room"}
+      </text>
+      <text
+        transform="rotate(45 684.608 655)"
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={662}
+        x={653.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Admin"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={676}
+        x={403.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Weapons"}
+      </text>
+      <text
+        transform="rotate(45 434.608 494)"
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={501}
+        x={353.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Communications"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={487}
+        x={669.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Office"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={519}
+        x={181.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"O2"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={434}
+        x={469.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"2. CENTRAL"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={254}
+        x={704.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"3. NORTHEAST"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={634}
+        x={510.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"4. SOUTH"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={611}
+        x={186.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"5. SOUTHWEST"}
+      </text>
+      <text
+        fontWeight="bold"
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={242}
+        x={363.5}
+        strokeOpacity="null"
+        strokeWidth="null"
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"6. NORTHWEST"}
+      </text>
+      <text
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={60}
+        x={890}
+        strokeOpacity="null"
+        strokeWidth={0}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Vents"}
+      </text>
+      <text
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={95}
+        x={890}
+        strokeOpacity="null"
+        strokeWidth={0}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Common Tasks"}
+      </text>
+      <text
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={130}
+        x={890}
+        strokeOpacity="null"
+        strokeWidth={0}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Wiring Tasks"}
+      </text>
+      <text
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={60}
+        x={1090}
+        strokeOpacity="null"
+        strokeWidth={0}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Vent Connections"}
+      </text>
+      <text
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={95}
+        x={1090}
+        strokeOpacity="null"
+        strokeWidth={0}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Security Cameras"}
+      </text>
+      <text
+        fontFamily="Titillium Web, sans-serif"
+        fontSize={20}
+        y={130}
+        x={1090}
+        strokeOpacity="null"
+        strokeWidth={0}
+        stroke="#000"
+        fill="#fff"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        {"Camera Zones"}
+      </text>
+      <ellipse
+        ry={3}
+        rx={3.5}
+        cy={273.707}
+        cx={444}
+        fill="red"
+        preserveAspectRatio="xMidYMid meet"
+      />
+      <ellipse
+        ry={3}
+        rx={3.5}
+        cy={466.016}
+        cx={892.72}
+        fill="red"
+        preserveAspectRatio="xMidYMid meet"
+      />
+      <ellipse
+        ry={3}
+        rx={3.5}
+        cy={467.192}
+        cx={553.975}
+        fill="red"
+        preserveAspectRatio="xMidYMid meet"
+      />
+      <ellipse
+        ry={3}
+        rx={3.5}
+        cy={287.821}
+        cx={787.45}
+        fill="red"
+        preserveAspectRatio="xMidYMid meet"
+      />
+      <ellipse
+        ry={3}
+        rx={3.5}
+        cy={587.752}
+        cx={595.141}
+        fill="red"
+        preserveAspectRatio="xMidYMid meet"
+      />
+      <ellipse
+        ry={3}
+        rx={3.5}
+        cy={650.091}
+        cx={266.394}
+        fill="red"
+        preserveAspectRatio="xMidYMid meet"
+      />
+    </svg>
+  );
+}
