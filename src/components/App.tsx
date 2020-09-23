09e8a9d71a1398281f5fd95cf3f79888d2592357
Kedyn Macedonio <mkedyn@gmail.com>
2020-09-22 17:53:18 -0700

    Developer notes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import { JssProvider, ThemeProvider } from "react-jss";
 import ControlsContent from "./ControlsContent";
 import MainContent from "./MainContent";
 import MapsContent from "./MapsContent";
+import Modal from "./common/Modal";
 import React from "react";
 import jssSetUp from "utils/jssSetUp";
 import { useData } from "context";
@@ -10,6 +11,7 @@ import { useData } from "context";
 export default function App(): JSX.Element {
   // eslint-disable-next-line
   const { theme } = useData()!;
+  const [showNotes, setShowNotes] = React.useState(false);
 
   return (
     <React.Fragment>
@@ -24,7 +26,15 @@ export default function App(): JSX.Element {
             <footer>
               <small>
                 fusliez notes{" "}
-                <a href="https://github.com/Kedyn/fusliez-notes/releases/tag/v0.7.0">
+                <a
+                  href="https://github.com/Kedyn/fusliez-notes/releases/tag/v0.7.0"
+                  onClick={(
+                    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+                  ) => {
+                    event.preventDefault();
+                    setShowNotes(!showNotes);
+                  }}
+                >
                   v0.8.0 (Preview)
                 </a>{" "}
                 [9/22/2020] made with &#10084; by the{" "}
@@ -34,6 +44,14 @@ export default function App(): JSX.Element {
                 .
               </small>
             </footer>
+            <Modal
+              title="Developer Notes"
+              show={showNotes}
+              onClose={() => setShowNotes(false)}
+            >
+              This is only a preview version. Although the current does not
+              allow theme color changes, it will be on the full 0.8.0 version
+            </Modal>
           </React.Suspense>
         </ThemeProvider>
       </JssProvider>
