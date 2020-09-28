e56f731da7f89d3010d20277bdc13e9607172cc7
francistse-realm <wtse.ucsd@gmail.com>
2020-09-27 17:49:48 -0700

    Fixed notes styling in mobile.

--- src/components/Notes/Notes.styles.ts
+++ src/components/Notes/Notes.styles.ts
@@ -4,12 +4,13 @@ import { createUseStyles } from "react-jss";
 export default createUseStyles((theme: ITheme) => ({
   root: (props) => ({
     margin: props.isMobile ? "0.5rem 1rem" : "",
+    height: "100%",
   }),
   notes: (props) => ({
     fontSize: "1.25rem",
+    flex: 1,
     width: "100%",
-    minHeight: props.isMobile ? "60vh" : "350px",
-    maxHeight: props.isMobile ? "60vh" : "",
+    minHeight: props.isMobile ? "55vh" : "350px",
     height: "100%",
     backgroundColor: theme.neutralBackgroundColor,
   }),
