391dbf3de3f1ba2dad6f21cde633a1f85644e100
Kevin Han <kevinhanyd@gmail.com>
2020-10-10 12:47:56 -0400

    Added Webpack and loaders as dependencies. Created Webpack configurations. Updated ESLint to override TypeScript checks on .js files.

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    // filename: "[name].[contenthash].js",
+    filename: "bundle.min.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, "src", "assets"),
+      components: path.resolve(__dirname, "src", "components"),
+      context: path.resolve(__dirname, "src", "context"),
+      themes: path.resolve(__dirname, "src", "themes"),
+      utils: path.resolve(__dirname, "src", "utils"),
+    },
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets" }],
+    }),
+  ],
+};
