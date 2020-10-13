695930012da68aaf5cade3197bc3a355c4de0a57
Daniel Singer <daniel@chilblane.com>
2020-10-12 20:28:14 -0700

    metadata added, and copy task updated

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,29 +1,21 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const JsonMinifyPlugin = require("node-json-minify");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 const isProduction = process.env.NODE_ENV === "production";
 
 const copyWebpackPlugin = new CopyWebpackPlugin({
   patterns: [
     {
-      from: "**/*.(jpe?g|png|gif|ico)",
+      from: "**/*.(jpe?g|png|gif)",
       to: "assets",
       context: "src/assets",
     },
     {
-      from: "**/*.(json|webmanifest)",
-      to: "assets",
-      context: "src/assets",
-      transform: function (content) {
-        if (isProduction) {
-          return JsonMinifyPlugin(content.toString());
-        }
-        return content;
-      },
+      from: "**/*.*",
+      to: ".",
+      context: "src/static",
     },
   ],
 });
@@ -36,14 +28,4 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
 
 const cleanWebpackPlugin = new CleanWebpackPlugin();
 
-const bundleAnalyzerPlugin = new BundleAnalyzerPlugin({
-  analyzerMode: "static",
-  openAnalyzer: false,
-});
-
-module.exports = [
-  cleanWebpackPlugin,
-  copyWebpackPlugin,
-  htmlWebpackPlugin,
-  bundleAnalyzerPlugin,
-];
+module.exports = [cleanWebpackPlugin, copyWebpackPlugin, htmlWebpackPlugin];
