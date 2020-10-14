e3b8c3e3631d9516f676a04f38950267d7ba6304
Kedyn Macedonio <mkedyn@gmail.com>
2020-10-14 06:04:46 -0700

    Added DeviceSlice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { Store, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
+import DeviceSlice from "./slices/DeviceSlice";
 import SettingsSlice from "./slices/SettingsSlice";
 
 const store: Store = configureStore({
   reducer: {
     settings: SettingsSlice.reducer,
+    device: DeviceSlice.reducer,
   },
   middleware: [...getDefaultMiddleware()],
 });
