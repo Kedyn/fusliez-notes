453f71257a4de2c1f8e1017a31e41c219f149908
Kedyn Macedonio <mkedyn@gmail.com>
2020-10-14 06:08:15 -0700

    Added ScoresSlice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import { Store, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import DeviceSlice from "./slices/DeviceSlice";
+import ScoresSlice from "./slices/ScoresSlice";
 import SettingsSlice from "./slices/SettingsSlice";
 
 const store: Store = configureStore({
   reducer: {
     settings: SettingsSlice.reducer,
     device: DeviceSlice.reducer,
+    scores: ScoresSlice.reducer,
   },
   middleware: [...getDefaultMiddleware()],
 });
