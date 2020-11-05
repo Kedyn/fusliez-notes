155cbe83ca255b529c52b6cf7288bfefe3481a17
Kedyn Macedonio <mkedyn@gmail.com>
2020-11-05 04:55:46 -0800

    Fixes #103 Deep import all icons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,3 @@
-import { faSort, faUsers } from "@fortawesome/free-solid-svg-icons";
-
 import ContextWrapper from "context";
 import Layout from "./Layout";
 import React from "react";
@@ -10,8 +8,10 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons/faHeart";
 import { faMap } from "@fortawesome/free-solid-svg-icons/faMap";
 import { faMinus } from "@fortawesome/free-solid-svg-icons/faMinus";
 import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
+import { faSort } from "@fortawesome/free-solid-svg-icons/faSort";
 import { faTimes } from "@fortawesome/free-solid-svg-icons/faTimes";
 import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons/faUserAstronaut";
+import { faUsers } from "@fortawesome/free-solid-svg-icons/faUsers";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 // TODO: deep imports aren't necessary.
