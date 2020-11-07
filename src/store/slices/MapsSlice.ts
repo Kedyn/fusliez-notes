aff0ef83aa319df4d4b04cccdc96e2ce9f4cb697
Kedyn Macedonio <mkedyn@gmail.com>
2020-11-06 22:26:13 -0800

    Fixes #100 Switched maps state to redux

---
+++ src/store/slices/MapsSlice.ts
@@ -0,0 +1,63 @@
+import { IMaps, IMapsCharacter, IUIStoreState } from "utils/types";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+import { DEFAULT_MAPS_DATA } from "constants/maps";
+
+const MapsSlice = createSlice({
+  name: "Maps",
+  initialState: DEFAULT_MAPS_DATA,
+  reducers: {
+    setCurrentMap: (state: IMaps, action: PayloadAction<string>) => ({
+      ...state,
+
+      currentMap: action.payload,
+    }),
+
+    setCharacterPosition: (
+      state: IMaps,
+      action: PayloadAction<IMapsCharacter>
+    ) => ({
+      ...state,
+
+      characters: [
+        ...state.characters.map((character) => {
+          if (character.id === action.payload.id) {
+            return { ...action.payload };
+          }
+
+          return character;
+        }),
+      ],
+    }),
+
+    resetCharacters: (state: IMaps) => ({
+      ...state,
+
+      characters: [
+        ...DEFAULT_MAPS_DATA.characters.map((character) => ({ ...character })),
+      ],
+    }),
+
+    reset: () => ({
+      currentMap: DEFAULT_MAPS_DATA.currentMap,
+      characters: [
+        ...DEFAULT_MAPS_DATA.characters.map((character) => ({ ...character })),
+      ],
+    }),
+  },
+});
+
+export const {
+  setCurrentMap,
+  setCharacterPosition,
+  resetCharacters,
+  reset,
+} = MapsSlice.actions;
+
+export const getCurrentMap = (state: IUIStoreState): string =>
+  state.Maps.currentMap;
+
+export const getCharacters = (state: IUIStoreState): Array<IMapsCharacter> =>
+  state.Maps.characters;
+
+export default MapsSlice;
