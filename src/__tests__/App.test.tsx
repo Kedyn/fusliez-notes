6f425efcd550e30d6d1e11bdab1dad947ad3d29a
francistse-realm <wtse.ucsd@gmail.com>
2021-03-10 20:01:33 -0800

    Fixed devDependencies
    Added test to test 'Understand' for privacy notice

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,14 +1,15 @@
 import "regenerator-runtime/runtime";
 
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import App from "../components/App";
 import { I18nextProvider } from "react-i18next";
+import { NAMESPACE } from "constants/main";
 import React from "react";
 import i18n from "../i18n";
 
 describe("App", () => {
-  test("checks if disclaimer is on the page", async () => {
+  test("disclaimer should be on the page", async () => {
     const { getByText } = await render(
       <I18nextProvider i18n={i18n}>
         <App />
@@ -20,5 +21,23 @@ describe("App", () => {
     );
 
     expect(disclaimer).toBeInTheDocument();
+  });
+
+  test("disclaimer should NOT be on the page", async () => {
+    const { getByRole } = await render(
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    );
+
+    const understandButton = await getByRole("button", {
+      name: /main.understand/i,
+    });
+    fireEvent.click(understandButton);
+
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(
+      `${NAMESPACE}disclaimer`,
+      "Understood"
+    );
   });
 });
---
+++ src/__tests__/setupTests.ts
@@ -0,0 +1,1 @@
+import "jest-canvas-mock"; // cant add to setupFiles in jest.config
