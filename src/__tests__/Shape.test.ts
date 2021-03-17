ce02e9c4dea0451e0fe9c69e648fb054ea74f410
francistse-realm <wtse.ucsd@gmail.com>
2021-03-16 23:07:25 -0700

    Added tests for Shape
    similar to Vector test, cant achieve 100% coverage since the setPosition function is missing the final else statement

---
+++ src/__tests__/Shape.test.ts
@@ -0,0 +1,28 @@
+import { Shape } from "utils/math/Shape";
+import { Vector } from "utils/math/Vector";
+
+describe("Shape tests", () => {
+  const shape = new Shape(new Vector(50, 100));
+
+  test("getPosition function should return current position", () => {
+    expect(shape.getPosition()).toStrictEqual(new Vector(50, 100));
+  });
+
+  test("getX function should return current X", () => {
+    expect(shape.getX()).toBe(50);
+  });
+
+  test("getY function should return current Y", () => {
+    expect(shape.getY()).toBe(100);
+  });
+
+  test("set position with 2 nums as args should set the current position to x and y, respectively", () => {
+    shape.setPosition(-20, -62);
+    expect(shape.getPosition()).toStrictEqual(new Vector(-20, -62));
+  });
+
+  test("set position with the arg being a Vector should set the current position to the vector", () => {
+    shape.setPosition(new Vector(1000, 612));
+    expect(shape.getPosition()).toStrictEqual(new Vector(1000, 612));
+  });
+});
