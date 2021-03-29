660b95906db123d87425fc2bb6f3801e25e8f619
francistse-realm <wtse.ucsd@gmail.com>
2021-03-28 19:32:06 -0700

    Added tests for colorConverter
    100% coverage

--- src/__tests__/colorConverter.test.ts
+++ src/__tests__/colorConverter.test.ts
@@ -4,18 +4,30 @@ import { COLOR_LIBRARY } from "constants/theme";
 
 describe("colorConverter tests", () => {
   test("getColorValue('teal') should return '0, 0, 0' ", () => {
-    const rgb = getColorValue("teal");
-
-    expect(rgb).toBe("0, 0, 0");
+    expect(getColorValue("teal")).toBe("0, 0, 0");
   });
 
   test("getColorValue('red') should return '197,17,17' ", () => {
-    const rgb = getColorValue("red");
-    expect(rgb).toBe("197,17,17");
+    expect(getColorValue("red")).toBe("197,17,17");
   });
 
   test("getColorValue('red', 'dark') should return '123,8,56' ", () => {
-    const rgb = getColorValue("red", "dark");
-    expect(rgb).toBe("123,8,56");
+    expect(getColorValue("red", "dark")).toBe("123,8,56");
+  });
+
+  test("lightenDarkenColor('#EF7D0E', 30) should return '#ff9b2c' ", () => {
+    expect(lightenDarkenColor("#EF7D0E", 30)).toBe("#ff9b2c");
+  });
+
+  test("lightenDarkenColor('EF7D0E', 30) should return 'ff9b2c' ", () => {
+    expect(lightenDarkenColor("EF7D0E", 30)).toBe("ff9b2c");
+  });
+
+  test("lightenDarkenColor('#EF7D0E', 200) should return '#ffffd6' ", () => {
+    expect(lightenDarkenColor("#EF7D0E", 200)).toBe("#ffffd6");
+  });
+
+  test("lightenDarkenColor('#EF7D0E', -300) should return '#0' ", () => {
+    expect(lightenDarkenColor("#EF7D0E", -300)).toBe("#0");
   });
 });
