13783e93c32576321b15dd8e998fb8abc986e38b
francistse-realm <wtse.ucsd@gmail.com>
2021-03-30 21:14:55 -0700

    Added test to cover new statement in Rectangle

--- src/__tests__/Rectangle.test.ts
+++ src/__tests__/Rectangle.test.ts
@@ -6,23 +6,23 @@ describe("Rectangle tests", () => {
   const rect = new Rectangle(vector, 50, 100);
 
   test("getWidth() should get Rectangle's width (=== 50)", () => {
-    expect(rect.getWidth()).toBe(50);
+    expect(rect.getWidth()).toEqual(50);
   });
 
   test("getHeight() should get Rectangle's height (=== 100)", () => {
-    expect(rect.getHeight()).toBe(100);
+    expect(rect.getHeight()).toEqual(100);
   });
 
   test("setWidth(100) should set Rectangle's width to 100", () => {
     rect.setWidth(100);
 
-    expect(rect.getWidth()).toBe(100);
+    expect(rect.getWidth()).toEqual(100);
   });
 
   test("setHeight(50) should set Rectangle's height to 50", () => {
     rect.setHeight(50);
 
-    expect(rect.getHeight()).toBe(50);
+    expect(rect.getHeight()).toEqual(50);
   });
 
   test("isPointInside should return true if a given point is inside the rectangle", () => {
@@ -34,5 +34,11 @@ describe("Rectangle tests", () => {
     rect.setHeight(20);
 
     expect(rect.isPointInside(new Vector(200, 300))).toBeFalsy();
+  });
+
+  test("setDimensions should set the rectangle's new dimensions", () => {
+    rect.setDimensions(200, 500);
+    expect(rect.getWidth()).toEqual(200);
+    expect(rect.getHeight()).toEqual(500);
   });
 });
